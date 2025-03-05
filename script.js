ca36a67a6d6b48b9b0511fd862bba13bd1393a94
Keven Willians <kevenscha@gmail.com>
2025-03-05 04:54:25 -0300

    update site

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let listaDeAmigos = [];
 
+
+
 //função para adicionar cada nome digitado dentro do array
 function adicionarAmigos(){
     let amigos = document.getElementById("amigo").value.trim();
@@ -59,5 +61,9 @@ function gerarNumeroAleatorio(){
     return numeroAleatorio;
 }
 
+
+function submitEmail() {
+    alert("Não há lógica, ainda não aprendi kk ;-;");
+}
 
 
